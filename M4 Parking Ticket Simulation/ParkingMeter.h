2e8aea36ea1178e7c5ad7a4f76f29e62208f7278
garrisonglass <garrison.glass@gmail.com>
2026-04-02 19:51:48 -0400

    added parkingMater.h

---
+++ M4 Parking Ticket Simulation/ParkingMeter.h
@@ -0,0 +1,17 @@
+#ifndef PARKINGMETER_H
+#define PARKINGMETER_H
+
+class ParkingMeter
+{
+	private:
+		int minutesPurchased;
+public:	
+	//Constructor
+	ParkingMeter(int minutesPurchased = 0);
+
+	//Accessor
+	int getMinutesPurchased() const { return minutesPurchased; }
+
+};
+
+#endif
